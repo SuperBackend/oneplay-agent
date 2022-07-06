8f6984a41c164cdb2651f1a8f4824e86ab9c1c40
acentior <acentior@outlook.com>
2022-07-06 17:33:13 +0400

    handler updated

--- internal/api/handler.go
+++ internal/api/handler.go
@@ -17,6 +17,42 @@ func handleError(w http.ResponseWriter, err error) {
 // MakeHandler returns an HTTP handler for the session service
 func MakeHandler(webrtc rtc.Service, display rdisplay.Service) http.Handler {
 	mux := http.NewServeMux()
+	mux.HandleFunc("/session", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+		dec := json.NewDecoder(r.Body)
+		req := newSessionRequest{}
+
+		if err := dec.Decode(&req); err != nil {
+			handleError(w, err)
+			return
+		}
+
+		peer, err := webrtc.CreateRemoteScreenConnection(req.Screen, 20)
+		if err != nil {
+			handleError(w, err)
+			return
+		}
+
+		answer, err := peer.ProcessOffer(req.Offer)
+
+		if err != nil {
+			handleError(w, err)
+			return
+		}
+
+		payload, err := json.Marshal(newSessionResponse{
+			Answer: answer,
+		})
+		if err != nil {
+			handleError(w, err)
+			return
+		}
+
+		w.Write(payload)
+	})
 
 	mux.HandleFunc("/screens", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method != http.MethodGet {
